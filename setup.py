7399a04d6bf116dec377d454134df3472f200acb
Augustin ROCHE <aroche@rnf.local>
2018-08-24 11:02:42 +0200

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name = "taxon_parser",
-    version = "0.2.0",
+    version = "0.2.1",
     author = "Augustin Roche",
     author_email = "aroche@photoherbarium.fr",
     description = "A library to parse taxon names into elementary components",
